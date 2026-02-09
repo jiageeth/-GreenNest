61157707d4fc2d8c474489061b8256744ae7973e
Jilsiya_ali <156494944+jiageeth@users.noreply.github.com>
2026-02-09 22:56:05 +0530

    add js files code

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+let header = document.querySelector('.header-3');
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('fa-xmark');
+    menu.classList.toggle('fa-bars');
+    navbar.classList.toggle('active');
+});
+
+window.addEventListener('scroll', () => {
+    menu.classList.remove('fa-xmark');
+    menu.classList.add('fa-bars');
+    navbar.classList.remove('active');
+
+    if (window.scrollY > 250) {
+        header.classList.add('active');
+    } else {
+        header.classList.remove('active');
+    }
+});
 
